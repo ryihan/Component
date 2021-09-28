d7b123a5ab6008eb236a6ee04c6f290de692a0c1
raihan <ryihan55@gmail.com>
2021-09-28 10:54:15 +0600

    Update WebComponent.js

--- TestProgram/Beta-1/WebComponent.js
+++ TestProgram/Beta-1/WebComponent.js
@@ -6,3 +6,10 @@
  **/
 
 import React from "react";
+
+const WebComponent = () => (
+  <svg width={1920} height={1080} viewBox="0 0 1920 1080">
+    <defs>
+      <style>
+        {
+          ".a,.f{fill:none;}.b{clip-path:url(#d);}.c{clip-path:url(#a);}.d{fill:#ff2323;stroke:#fb0000;}.e{clip-path:url(#b);}.f{stroke:#707070;}.g,.j{fill:#fff;}.h,.i{stroke:none;}.i{fill:#707070;}"
