39771022625c8ad04450c8650c32e4e0348d4fd9
raihan <ryihan55@gmail.com>
2021-09-28 11:03:44 +0600

    Update RepeatGridComponent.stories.js

--- TestProgram/Beta-1/RepeatGridComponent.stories.js
+++ TestProgram/Beta-1/RepeatGridComponent.stories.js
@@ -5,3 +5,27 @@
  *
  **/
 
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+import RepeatGrid from "./RepeatGridComponent";
+
+export const myProps = {
+  id: "1",
+  className: "mySampleClass",
+  sampleString: "Test Task",
+  sampleDate: new Date(2018, 0, 1, 9, 0)
+};
+
+export const actions = {
+  onActionOne: action("onActionOne"),
+  onActionTwo: action("onActionTwo")
+};
+
+storiesOf("RepeatGrid", module)
+  .add("default", () => <RepeatGrid {...myProps} {...actions} />)
+  .add("pinned", () => <RepeatGrid {...myProps} pinned={true} {...actions} />)
+  .add("archived", () => (
+    <RepeatGrid {...myProps} archived={true} {...actions} />
+  ));
