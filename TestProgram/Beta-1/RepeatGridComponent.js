500b55faa2a663a3d3b23657ccf36e9b53c6da5e
raihan <ryihan55@gmail.com>
2021-09-28 11:03:52 +0600

    Create RepeatGridComponent.js

---
+++ TestProgram/Beta-1/RepeatGridComponent.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+
+const RepeatGridComponent = () => (
+  <svg width={1177} height={1054} viewBox="0 0 1177 1054">
+    <defs>
+      <style>
+        {
+          ".a{fill:none;}.b{clip-path:url(#a);}.c{fill:#ff2323;stroke:#fb0000;}.d{stroke:none;}"
