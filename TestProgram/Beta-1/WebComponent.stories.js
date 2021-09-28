33d14857cdece149e239bc978a84b3d199bdae04
raihan <ryihan55@gmail.com>
2021-09-28 10:55:28 +0600

    Create WebComponent.stories.js

---
+++ TestProgram/Beta-1/WebComponent.stories.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
