5fdccef9c92097c94bfd7e02e7b66c5ec618127a
raihan <ryihan55@gmail.com>
2021-09-28 10:56:24 +0600

    Update WebComponent.stories.js

--- TestProgram/Beta-1/WebComponent.stories.js
+++ TestProgram/Beta-1/WebComponent.stories.js
@@ -15,3 +15,15 @@ export const myProps = {
   id: "1",
   className: "mySampleClass",
   sampleString: "Test Task",
+  sampleDate: new Date(2018, 0, 1, 9, 0)
+};
+
+export const actions = {
+  onActionOne: action("onActionOne"),
+  onActionTwo: action("onActionTwo")
+};
+
+storiesOf("Web", module)
+  .add("default", () => <Web {...myProps} {...actions} />)
+  .add("pinned", () => <Web {...myProps} pinned={true} {...actions} />)
+  .add("archived", () => <Web {...myProps} archived={true} {...actions} />);
