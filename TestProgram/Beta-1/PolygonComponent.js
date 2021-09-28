0488bb2abe6c43eb2b631bfe08d80cf41c7beca2
raihan <ryihan55@gmail.com>
2021-09-28 11:01:14 +0600

    Create PolygonComponent.js

---
+++ TestProgram/Beta-1/PolygonComponent.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * This file was generated with Adobe XD React Exporter
+ * Exporter for Adobe XD is written by: Johannes Pichler <j.pichler@webpixels.at>
+ *
+ **/
+
+import React from "react";
+
+const PolygonComponent = () => (
+  <svg width={1125} height={409} viewBox="0 0 1125 409">
+    <defs>
+      <style>{".a{fill:#fff;}.b,.c{stroke:none;}.c{fill:#707070;}"}</style>
+    </defs>
+    <g className="a">
+      <path
+        className="b"
+        d="M 1123.462158203125 408.5 L 1.5378657579422 408.5 L 562.5 0.6181992292404175 L 1123.462158203125 408.5 Z"
+      />
+      <path
+        className="c"
+        d="M 562.5 1.23651123046875 L 3.075927734375 408 L 1121.924072265625 408 L 562.5 1.23651123046875 M 562.5 0 L 1125 409 L 0 409 L 562.5 0 Z"
+      />
+    </g>
+  </svg>
+);
+
+export default PolygonComponent;
