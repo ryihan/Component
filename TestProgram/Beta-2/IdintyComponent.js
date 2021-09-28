2862098cb6e46668a0c63b7b2b8190326f12a692
raihan <ryihan55@gmail.com>
2021-09-28 23:29:50 +0600

    Update IdintyComponent.js

--- TestProgram/Beta-2/IdintyComponent.js
+++ TestProgram/Beta-2/IdintyComponent.js
@@ -6,3 +6,9 @@
  **/
 
 import React from "react";
+const IdintyComponent = () => (
+  <svg width={1920} height={1080} viewBox="0 0 1920 1080">
+    <defs>
+      <style>{".a{clip-path:url(#c);}.b{fill:url(#a);}"}</style>
+      <pattern
+        id="a"
